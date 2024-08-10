6d140ec6bec2a91f746920bdfe7316f84597c2d2
FrancescoMnv <62492037+francesco-1987@users.noreply.github.com>
2024-08-10 13:01:28 -0700

    Change image and deploy

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ st.set_page_config(layout="wide")
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image('images/photo.png', use_column_width=True)
+    st.image('images/photo.jpg', use_column_width=True)
 
 with col2:
-    st.title("Francesco Zerbato")
-    content = """ Francesco e' un campione"""
+    st.title("Francesco")
+    content = """ Francesco corre"""
 
     st.info(content)
 st.write("This content follows a single-column format. Please notice it")
